f23a30cbb6275e56d45e3b9457a7315c1735d9a8
Messias <fmessiasca@gmail.com>
2024-07-02 17:26:13 -0400

    Criação de rotas, método GET, estudos sobre Route Params e Query Params

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
-import express from 'express';
-
 const express = require('express');
 
 const app = express();
 
-const PORT = 3333;
+/* Route Params*/
+app.get('/polls', (request, response) => {
+    response.send(`Escolha uma enquete ou crie uma nova.`)
+});
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.get("/polls/:user/:id", (req, res) => {
+    const { user, id } = req.params;
+
+    res.send(`Você está na enquete do ${user} que é sobre ${id}.`)
+});
+
+/* Query Params */
+app.get("/enquete", (request, response) => {
+    const {criador, assunto} = request.query;
+
+    response.send(`Você está na enquete do ${criador} que é sobre ${assunto}.`)
+});
+
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+
+
